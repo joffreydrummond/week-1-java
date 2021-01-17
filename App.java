9933dce94afa8b906ee0a140ef549733b5e0edf0
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-01-17 10:42:31 -0700

    added public static void main method

--- App.java
+++ App.java
@@ -1,20 +1,28 @@
-//package week-1-java;
-
 public class App {
-    float itemPrice = 3.99f;
-    int moneyInWallet = 20;
 
-    int numOfFriends = 4;
-    final int ageInYears = 36;
+    public static void main(String[] args) {
 
-    String firstName = "Joffrey";
-    String lastName = "Drummond";
-    String middleName = "Edward";
+        float itemPrice = 3.99f;
+        int moneyInWallet = 20;
 
-    float remainingMoneyInWallet = moneyInWallet - itemPrice;
+        int numOfFriends = 4;
+        final int ageInYears = 36;
 
-    int friendsAvg = ageInYears / numOfFriends;
+        String firstName = "Joffrey";
+        String lastName = "Drummond";
+        String middleName = "Edward";
 
-    String fullName = firstName + middleName + lastName;
+        float remainingMoneyInWallet = moneyInWallet - itemPrice;
+
+        int friendsAvg = ageInYears / numOfFriends;
+
+        String fullName = firstName + middleName + lastName;
+
+        System.out
+                .println("After buying the combo meal, I only have " + remainingMoneyInWallet + " left in my wallet!");
+        System.out.println("I have  " + friendsAvg + " really close friends.");
+        System.out.println("My full name is " + fullName);
+
+    }
 
 }
