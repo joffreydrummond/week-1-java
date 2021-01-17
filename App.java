b442aaac173af33e1b622b20a54da8016fde60c6
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-01-17 09:00:59 -0700

    added variables

---
+++ App.java
@@ -0,0 +1,14 @@
+//package week-1-java;
+
+public class App {
+    float itemPrice = 3.99f;
+    int moneyInWallet = 20;
+
+    int numOfFriends = 4;
+    int ageInYears = 36;
+
+    String firstName = "Joffrey";
+    String lastName = "Drummond";
+    String middleName = "Edward";
+
+}
